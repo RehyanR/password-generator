3e12cb2292a79b9d21ed848ace9a9ec01a81bf12
RehyanR <151837449+RehyanR@users.noreply.github.com>
2024-03-06 18:53:03 +0000

    sorted Syntax Error

--- starter/script.js
+++ starter/script.js
@@ -90,7 +90,7 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  return array[Math.floor(Math.random() * arr.length)];
 }
 
 // Function to generate password with user input
